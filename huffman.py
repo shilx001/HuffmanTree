f38affbab8672dcd44562c9dff1118f8dd528a3a
shilx001 <shilx001@gmail.com>
2021-05-14 16:43:50 +0800

    Implement huffman tree.

--- huffman.py
+++ huffman.py
@@ -42,10 +42,15 @@ class HuffmanTree:
         self.tree_copy = self.tree.copy()
         self.buildTree()
         self.codebook = self.getCode()
-        self.input_state = tf.placeholder(dtype=tf.float32, shape=[1, self.max_seq_length, state_dim])
+        self.input_state = tf.placeholder(dtype=tf.float32, shape=[None, self.max_seq_length, state_dim])
         self.input_state_length = tf.placeholder(dtype=tf.float32, shape=[None, ])
         self.input_reward = tf.placeholder(dtype=tf.float32, shape=[None, ])
-        self.buildNetwork_v1()  # 构建网络
+        self.input_action = tf.placeholder(dtype=tf.int32, shape=[None, ])
+        self.buildNetwork_v2()  # 构建网络
+        self.action_prob_list = self.build_tree_embedding()
+        action_prob = tf.nn.embedding_lookup(self.action_prob_list, self.input_action)
+        self.loss = -tf.reduce_mean(self.input_reward * tf.log(action_prob + 1e-13))
+        self.train_op = tf.train.AdamOptimizer(self.lr).minimize(self.loss)
         self.sess = tf.Session()
         self.sess.run(tf.global_variables_initializer())
 
@@ -90,11 +95,45 @@ class HuffmanTree:
         Create RNN feature extractor for recommendation systems.
         :return:
         '''
-        with tf.variable_scope('feature_extract', reuse=False):
+        with tf.variable_scope('feature_extract', reuse=tf.AUTO_REUSE):
             basic_cell = tf.contrib.rnn.GRUCell(num_units=self.hidden_size)
             _, states = tf.nn.dynamic_rnn(basic_cell, input_state, dtype=tf.float32,
                                           sequence_length=input_state_length)
         return states
+
+    def feature_extract_atem(self, input_state, input_state_length):
+        '''
+        build the ATEM model
+        :param input_state: input state.
+        :param input_length: input state length
+        :return: the ATEM model
+        '''
+        with tf.variable_scope('feature_extractor_atem', reuse=False):
+            item_embedding = slim.fully_connected(input_state, self.hidden_size)  # [N, max_seq_length,hidden_size]
+            attention_layer = slim.fully_connected(item_embedding, 1)
+            attention_weight = tf.nn.softmax(attention_layer, 1)  # [N, max_seq_length, 1]
+            output_layer = attention_weight * item_embedding  # [N, 1, hidden_size]
+            output_layer = tf.reduce_sum(output_layer, axis=1)
+        return output_layer
+
+    def feature_extract_caser(self, input_state, input_state_length):
+        with tf.variable_scope('feature_extract_caser', reuse=False):
+            input_state = tf.expand_dims(input_state, axis=3)
+            output_v = tf.layers.conv2d(input_state, self.hidden_size, [self.max_seq_length, 1],
+                                        activation=tf.nn.relu)
+            out_v = tf.layers.flatten(output_v)  # [N, self.state_dim*self.hidden_size]
+            output_hs = list()
+            for h in np.arange(self.max_seq_length) + 1:
+                conv_out = tf.layers.conv2d(input_state, self.hidden_size, [h, self.state_dim],
+                                            activation=tf.nn.relu)
+                conv_out = tf.reshape(conv_out, [-1, self.max_seq_length - h + 1, self.hidden_size])
+                pool_out = tf.layers.max_pooling1d(conv_out, [self.max_seq_length + 1 - h], 1)
+                pool_out = tf.squeeze(pool_out, 1)
+                output_hs.append(pool_out)
+            out_h = tf.concat(output_hs, axis=1)
+            out = tf.concat([out_v, out_h], axis=1)
+            z = tf.layers.dense(out, self.hidden_size, activation=tf.nn.relu)
+        return z
 
     def mlp(self, id=None, softmax_activation=False):
         '''
@@ -104,7 +143,7 @@ class HuffmanTree:
         :return: a multi-layer neural network with output dim equals to branch size.
         '''
         with tf.variable_scope('node_' + str(id), reuse=tf.AUTO_REUSE):
-            state = self.feature_extract(self.input_state, self.input_state_length)
+            state = self.feature_extract_caser(self.input_state, self.input_state_length)
             l1 = slim.fully_connected(state, self.hidden_size)
             l2 = slim.fully_connected(l1, self.hidden_size)
             l3 = slim.fully_connected(l2, self.branch)
@@ -132,43 +171,55 @@ class HuffmanTree:
 
     def buildNetwork_v2(self):
         # Build tree-structured neural networks for each node, layer parameter sharing
-        # still have some bugs
         queue = []
         current_line = 0
         queue.append([current_line, self.tree])
         count = 0
+        networks = [self.mlp(id=str(count), softmax_activation=True)]
         while len(queue) > 0:
             line, node = queue.pop(0)
             if line != current_line:  # for parameter sharing
                 current_line = line
-            node.network = self.mlp(id=str(count), softmax_activation=True)
+                networks.append(self.mlp(id=str(count), softmax_activation=True))
+            node.network = networks[-1]
+            if len(node.child) != 0:
+                for n in node.child:
+                    queue.append([line + 1, n])
+                    count += 1
+
+    def buildNetwork_v3(self):
+        # Build tree-structured neural networks for each node, layer parameter sharing with extra decision unit
+        queue = []
+        current_line = 0
+        queue.append([current_line, self.tree])
+        count = 0
+        t_node = self.mlp(id=str(count), softmax_activation=False)
+        t_node = slim.fully_connected(t_node, num_outputs=self.branch, activation_fn=tf.nn.relu)
+        t_node = slim.fully_connected(t_node, num_outputs=self.branch, activation_fn=tf.nn.softmax)
+        networks = [t_node]
+        while len(queue) > 0:
+            line, node = queue.pop(0)
+            if line != current_line:  # for parameter sharing
+                current_line = line
+                t_node = self.mlp(id=str(count), softmax_activation=False)
+                t_node = slim.fully_connected(t_node, num_outputs=self.branch, activation_fn=tf.nn.relu)
+                t_node = slim.fully_connected(t_node, num_outputs=self.branch, activation_fn=tf.nn.softmax)
+                networks.append(t_node)
+            node.network = networks[-1]
             if len(node.child) != 0:
                 for n in node.child:
                     queue.append([line + 1, n])
                     count += 1
 
     def learn(self, state, state_length, action, reward):
-        # 根据输入的状态、动作和回报计算loss
-        # 先根据action找到对应的path
-        # 再根据path对路径进行遍历，计算总概率
-        # 根据概率得出
-        loss_list = []
-        for i in range(len(action)):
-            with tf.Session() as s:
-                #tf.reset_default_graph()
-                action_prob = self.getActionProb(action[i])
-                loss = -self.input_reward * tf.log(action_prob + 1e-13)
-                train_op = tf.train.AdamOptimizer(self.lr).minimize(loss)
-                # self.sess.run(tf.variables_initializer(tf.report_uninitialized_variables()))
-                s.run(tf.global_variables_initializer())
-                #self.sess.run(tf.variables_initializer([loss]))
-                _, l = s.run([train_op, loss],
-                                     feed_dict={
-                                         self.input_state: state[i].reshape([1, self.max_seq_length, self.state_dim]),
-                                         self.input_state_length: [state_length[i], ],
-                                         self.input_reward: [reward[i], ]})
-            loss_list.append(l)
-        return np.mean(loss_list)
+        state_length = state_length.flatten()
+        action = action.flatten()
+        reward = reward.flatten()
+        _, loss = self.sess.run([self.train_op, self.loss], feed_dict={
+            self.input_state: state,
+            self.input_state_length: state_length,
+            self.input_reward: reward, self.input_action: action})
+        return np.mean(loss)
 
     def getAction(self, state, state_length):
         # 根据输入的state和state_length得到一个动作的采样
@@ -218,21 +269,35 @@ class HuffmanTree:
             input_state = np.vstack((input_state, padding_mat))
         return input_state
 
+    def build_tree_embedding(self):
+        # 构建从id到variable的embedding
+        embedding = []
+        for i in range(len(self.id)):
+            embedding.append(self.getActionProb(i))
+        return tf.stack(embedding)
 
-id = np.arange(100)
-np.random.seed(1)
-frequency = np.arange(100)
-h_tree = HuffmanTree(value=frequency, id=id, branch=3, state_dim=10)
+    def get_all_action_prob(self, input_state, input_state_length):
+        input_state = np.reshape(input_state, [-1, self.max_seq_length, self.state_dim])
+        input_state_length = np.reshape(input_state_length, [-1, ])
+        return self.sess.run(self.action_prob_list, feed_dict={self.input_state: input_state,
+                                                               self.input_state_length: input_state_length})
 
-state = np.random.rand(1, 32, 10)
-state_length = 10
-action = h_tree.getAction(state, [state_length])
-action_prob = h_tree.getActionProb(1)
 
-for i in range(100):
-    start = datetime.datetime.now()
-    loss = h_tree.learn(state, [state_length], [1], [10])
-    end = datetime.datetime.now()
-    print('Step {}\n loss:{} time:{}'.format(i,loss,(end-start).seconds))
-print('Training time:{}'.format((end - start).seconds))
-pass
+# for debugging
+if __name__ == '__main__':
+    id = np.arange(100)
+    np.random.seed(1)
+    frequency = np.arange(100)
+    h_tree = HuffmanTree(value=frequency, id=id, branch=3, state_dim=10)
+
+    state = np.random.rand(1, 32, 10)
+    state_length = 10
+    action = h_tree.getAction(state, [state_length])
+    action_prob = h_tree.getActionProb(1)
+
+    for i in range(100):
+        start = datetime.datetime.now()
+        loss = h_tree.learn(state, [state_length], [1], [10])
+        end = datetime.datetime.now()
+        print('Step {}\n loss:{} time:{}'.format(i, loss, (end - start).seconds))
+    print('Training time:{}'.format((end - start).seconds))
---
+++ run_huffman.py
@@ -0,0 +1,242 @@
+import numpy as np
+import tensorflow as tf
+import pandas as pd
+import pickle
+from huffman import *
+import datetime
+import matplotlib.pyplot as plt
+from funk_svd import SVD
+
+model_name = 'GRU'
+np.random.seed(1)
+tf.set_random_seed(1)
+#data = pd.read_csv('ratings.csv', header=0, names=['u_id', 'i_id', 'rating', 'timestep'])
+data = pd.read_table('ratings.dat', sep='::', names=['u_id', 'i_id', 'rating', 'timestep'])
+#data=data[:100]
+
+user_idx = data['u_id'].unique()  # id for all the user
+np.random.shuffle(user_idx)
+train_id = user_idx[:int(len(user_idx) * 0.8)]
+test_id = user_idx[int(len(user_idx) * 0.8):]
+
+# Count the movies
+movie_id = []
+for idx1 in user_idx:  # 针对train_id中的每个用户
+    user_record = data[data['u_id'] == idx1]
+    for idx2, row in user_record.iterrows():
+        if row['i_id'] in movie_id:
+            idx = movie_id.index(row['i_id'])  # 找到该位置
+        else:
+            # 否则新加入movie_id
+            movie_id.append(row['i_id'])
+
+# Build user rating matrix
+rating_mat = np.zeros([len(train_id), len(movie_id)])
+movie_id = np.array(movie_id)
+for idx in train_id:  # 针对每个train数据
+    record = data[data['u_id'] == idx]  # record有多个数据，所以row_index也有多个
+    for _, row in record.iterrows():  # 针对每个用户的每条评分
+        r = np.where(train_id == idx)
+        c = np.where(row['i_id'] == movie_id)
+        rating_mat[r, c] = row['rating']
+
+# count the movies
+movie_total_rating = []
+movie_count = []
+movie_id = []
+for idx1 in user_idx:  # 针对train_id中的每个用户
+    user_record = data[data['u_id'] == idx1]
+    for idx2, row in user_record.iterrows():
+        # 检查list中是否有该电影的id
+        if row['i_id'] in movie_id:
+            idx = movie_id.index(row['i_id'])  # 找到该位置
+            movie_count[idx] += 1  # 计数加一
+            movie_total_rating[idx] += row['rating']
+        else:
+            # 否则新加入movie_id
+            movie_id.append(row['i_id'])
+            movie_count.append(1)
+            movie_total_rating.append(row['rating'])
+print('Total movie count:', len(movie_id))
+
+# Funk SVD for item representation
+train = data[data['u_id'].isin(train_id)]
+test = data[data['u_id'].isin(test_id)]
+svd = SVD(learning_rate=1e-3, regularization=0.005, n_epochs=200, n_factors=128, min_rating=0, max_rating=5)
+svd.fit(X=data, X_val=test, early_stopping=True, shuffle=False)
+item_matrix = svd.qi
+
+
+def get_feature(input_id):
+    # 根据输入的movie_id得出相应的feature
+    movie_index = np.where(movie_id == input_id)
+    return item_matrix[movie_index]
+
+
+def action_mapping(input_id):
+    '''
+    convert input movie id to index
+    :param input_id: movie id
+    :return: index of movie.
+    '''
+    return np.where(movie_id == input_id)
+
+
+def one_hot_rating(input_rating):
+    '''
+    convert the rating to one-hot code.
+    :param input_rating:
+    :return: one-hot code for ratings
+    '''
+    output_rating = np.zeros(11)
+    index = int(input_rating / 0.5)
+    output_rating[index] = 1
+    return output_rating
+
+
+max_seq_length = 32
+state_dim = item_matrix.shape[1] + 1
+hidden_size = 64
+
+agent = HuffmanTree(value=movie_count, id=list(range(len(movie_count))), state_dim=state_dim, branch=24,
+                    learning_rate=1e-4)
+
+
+def normalize(rating):
+    max_rating = 5
+    min_rating = 0
+    return -1 + 2 * (rating - min_rating) / (max_rating - min_rating)
+
+
+print('Begin training the tree policy.')
+start = datetime.datetime.now()
+discount_factor = 1
+train_step = 0
+loss_list = []
+for id1 in train_id:
+    user_record = data[data['u_id'] == id1]
+    state = []
+    rating = []
+    action = []
+    for _, row in user_record.iterrows():
+        movie_feature = get_feature(row['i_id'])
+        current_state = np.hstack((movie_feature.flatten(), (row['rating'])))
+        rating.append(row['rating'])
+        state.append(current_state)
+        action.append(action_mapping(row['i_id']))
+        if len(rating) % 32 is 0:
+            state_list = []
+            state_length_list = []
+            action_list = []
+            reward_list = []
+            for i in range(2, len(state)):
+                current_state = state[:i - 1]
+                current_state_length = i - 1
+                temp_state = agent.state_padding(current_state, current_state_length)
+                state_length_list.append(current_state_length)
+                state_list.append(temp_state)
+                action_list.append(action[i])
+                reward_list.append(normalize(rating[i]))  # normalization of the ratings to 0,1
+            discount = discount_factor ** np.arange(len(reward_list))
+            Q_value = np.cumsum(reward_list[::-1])
+            Q_value = Q_value[::-1] * discount
+            loss = agent.learn(state_list, np.array(state_length_list), np.array(action_list), Q_value)
+            loss_list.append(loss)
+            train_step += 1
+            print('Step ', train_step, 'Loss: ', loss)
+            state = []
+            rating = []
+            action = []
+end = datetime.datetime.now()
+training_time = (end - start).seconds
+
+print('Begin Test')
+test_count = 0
+result = []
+total_testing_steps = 0
+start = datetime.datetime.now()
+
+
+def evaluate(recommend_id, item_id, rating, top_N):
+    '''
+    evalute the recommend result for each user.
+    :param recommend_id: the recommend_result for each item, a list that contains the results for each item.
+    :param item_id: item id.
+    :param rating: user's rating on item.
+    :param top_N: N, a real number of N for evaluation.
+    :return: reward@N, recall@N, MRR@N
+    '''
+    session_length = len(recommend_id)
+    relevant = 0
+    recommend_relevant = 0
+    selected = 0
+    output_reward = 0
+    mrr = 0
+    for ti in range(session_length):
+        current_recommend_id = list(recommend_id[ti])[:top_N]
+        current_item = item_id[ti]
+        current_rating = rating[ti]
+        if current_rating > 3.5:
+            relevant += 1
+            if current_item in current_recommend_id:
+                recommend_relevant += 1
+        if current_item in current_recommend_id:
+            selected += 1
+            output_reward += normalize(current_rating)
+            rank = current_recommend_id.index(current_item)
+            mrr += 1.0 / (rank + 1)
+    recall = recommend_relevant / relevant if relevant is not 0 else 0
+    precision = recommend_relevant / selected if selected is not 0 else 0
+    return output_reward / session_length, precision, recall, mrr / session_length
+
+
+for id1 in test_id:
+    user_record = data[data['u_id'] == id1]
+    all_state = []
+    count = 0
+    all_recommend = []
+    all_item = []
+    all_rating = []
+    for _, row in user_record.iterrows():
+        movie_feature = get_feature(row['i_id'])
+        current_state = np.hstack((movie_feature.flatten(), (row['rating'])))
+        all_state.append(current_state)
+        if len(all_state) > 1:
+            count += 1
+            temp_state = all_state[:-1]
+            temp_state_length = len(temp_state)
+            temp_state = agent.state_padding(temp_state, temp_state_length)
+            output_action = agent.get_all_action_prob(temp_state, temp_state_length).flatten()
+            total_testing_steps += 1
+            output_action = output_action[:len(movie_id)]
+            recommend_idx = np.argsort(-output_action)[:100]
+            recommend_movie = [movie_id[_] for _ in recommend_idx]
+            all_recommend.append(recommend_movie)
+            all_item.append(row['i_id'])
+            all_rating.append(row['rating'])
+    reward_10, precision_10, recall_10, mkk_10 = evaluate(all_recommend, all_item, all_rating, 10)
+    reward_30, precision_30, recall_30, mkk_30 = evaluate(all_recommend, all_item, all_rating, 30)
+    test_count += 1
+    print('Test user #', test_count, '/', len(test_id))
+    print('Reward@10: %.4f, Precision@10: %.4f, Recall@10: %.4f, MRR@10: %4f'
+          % (reward_10, precision_10, recall_10, mkk_10))
+    print('Reward@30: %.4f, Precision@30: %.4f, Recall@30: %.4f, MRR@30: %4f'
+          % (reward_30, precision_30, recall_30, mkk_30))
+    result.append([reward_10, precision_10, recall_10, mkk_10, reward_30, precision_30, recall_30, mkk_30])
+end = datetime.datetime.now()
+testing_time = (end - start).seconds
+
+print('###############')
+print('Learning finished')
+print('Total training steps: {}'.format(train_step))
+print('Total learning time: {}'.format(training_time))
+print('Average learning time for each step: {:.5f}'.format(training_time / train_step))
+print('Total testing steps: {}'.format(total_testing_steps))
+print('Total testing time: {}'.format(testing_time))
+print('Average time per decision: {:.5f}'.format(testing_time / total_testing_steps))
+
+pickle.dump(result, open('huffman' + model_name, mode='wb'))
+print('Result:')
+display = np.mean(np.array(result).reshape([-1, 8]), axis=0)
+for num in display:
+    print('%.5f' % num)
